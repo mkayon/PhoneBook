a41d9937a812034ff27c2d26594c444a9e8b26e0
mkayon <mkayon@yandex.ua>
2017-05-31 03:40:33 +0300

    var patch

--- PhoneBook/Scripts/app/app-controller.js
+++ PhoneBook/Scripts/app/app-controller.js
@@ -158,17 +158,17 @@
               $http.get(folder_patch + '/api/Contacts1').success(callback);
           },
           ContactDetail: function (id, callback) {
-              $http.get('/api/Contacts1/' + id).success(callback);
+              $http.get(folder_patch + '/api/Contacts1/' + id).success(callback);
           },
           Deletecontact: function (id, callback) {
               $http({
                   method: 'DELETE',
-                  url:  '/api/Contacts1/' + $scope.data.contactId,
+                  url: folder_patch + '/api/Contacts1/' + $scope.data.contactId,
                   data: $scope.data, //forms user object
               }).success(callback );
           },
           Editcontact: function (data, callback) {
-              var posturl = '/api/Contacts1/' + data.contactId;
+              var posturl = folder_patch + '/api/Contacts1/' + data.contactId;
               $http({
                   method: 'PUT',
                   url: posturl,
